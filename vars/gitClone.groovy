87d8405436286bf60d6cb36c5b58e564b52272ee
sameer-shukur <sameer.shukur.m@gmail.com>
2020-12-06 12:38:12 +0530

    Add files via upload

---
+++ vars/gitClone.groovy
@@ -0,0 +1,3 @@
+def call(def url, def branch){
+checkout([$class: 'GitSCM', branches: [[name: "${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${url}"]]])
+}
